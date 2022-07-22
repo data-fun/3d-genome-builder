9a2d62b3ce81691bc4b261d7adcd5de85bbbfff5
Pierre Poulain <pierre.poulain@cupnet.net>
2022-07-22 17:24:50 +0200

    Clone the last commit only

--- envs/workflow.post-deploy.sh
+++ envs/workflow.post-deploy.sh
@@ -2,8 +2,8 @@
 # https://snakemake.readthedocs.io/en/stable/snakefiles/deployment.html
 
 # Clone pastis repo
-git clone https://github.com/hiclib/pastis.git
-# Copy required submodules
+git clone --depth 1 https://github.com/hiclib/pastis.git
+# Copy required submodules from the last commit
 cp -R pastis/pastis/dispersion "${CONDA_PREFIX}/lib/python3.9/site-packages/pastis/"
 cp -R pastis/pastis/optimization "${CONDA_PREFIX}/lib/python3.9/site-packages/pastis/"
 # Remove pastis repo
