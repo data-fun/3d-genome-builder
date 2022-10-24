26a593aba2ac030196c16c27b61893c990b91b20
Pierre Poulain <pierre.poulain@cupnet.net>
2022-10-24 11:18:56 +0200

    Set chromosome = residue = chain in PDB

--- scripts/assign_chromosomes.py
+++ scripts/assign_chromosomes.py
@@ -1,5 +1,8 @@
 """Annotate a PDB file containing a 3D genome structure with chromosome number.
 
+To ease visualization and further analysis:
+1 chromosome = 1 residue = 1 chain.
+
 It requires:
 - a PDB file containing the genome structure,
 - a FASTA file containing the genome sequence,
@@ -9,6 +12,7 @@ It requires:
 import argparse
 import math
 from pathlib import Path
+import string
 
 from Bio import SeqIO
 from biopandas.pdb import PandasPdb
@@ -42,30 +46,44 @@ def get_cli_arguments():
     argparse.Namespace
         Object containing arguments
     """
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
+    parser = argparse.ArgumentParser(add_help=False)
+    required = parser.add_argument_group("required arguments")
+    optional = parser.add_argument_group("optional arguments")
+    required.add_argument(
         "--pdb",
         action="store",
         type=lambda name: is_file(parser, name),
-        help="PDB file containing the 3D structure of the genome",
+        help="PDB file containing the 3D structure of the genome.",
         required=True,
     )
-    parser.add_argument(
+    required.add_argument(
         "--fasta",
         action="store",
         type=lambda name: is_file(parser, name),
-        help="Fasta file containing the sequence of the genome",
+        help="FASTA file containing the sequence of the genome.",
         required=True,
     )
-    parser.add_argument(
-        "--resolution", action="store", type=int, help="HiC resolution", required=True,
+    required.add_argument(
+        "--resolution",
+        action="store",
+        type=int,
+        help="HiC resolution from which the structure has been generated.",
+        required=True,
     )
-    parser.add_argument(
+    required.add_argument(
         "--output",
         action="store",
         type=str,
-        help="Output PDB file containing the annotated 3D structure of the genome",
+        help="Output PDB file containing the annotated 3D structure of the genome.",
         required=True,
+    )
+    # Add help.
+    optional.add_argument(
+        "-h",
+        "--help",
+        action="help",
+        default=argparse.SUPPRESS,
+        help="Show this help message and exit.",
     )
     return parser.parse_args()
 
@@ -76,12 +94,12 @@ def extract_chromosome_length(fasta_name):
     Parameters
     ----------
     fasta_name : str
-        Name of Fasta file containing the sequence of the genome
+        Name of Fasta file containing the sequence of the genome.
     
     Returns
     -------
     list
-        List of chromosome lengthes
+        List of chromosome lengths.
     """
     chromosome_length_lst = []
     with open(fasta_name, "r") as fasta_file:
@@ -101,21 +119,22 @@ def assign_chromosome_number(
     Note:
     - The PDB file produced by Pastis is not readable by Biopython
     because the residue number column is missing.
-    - We use instead the biopandas library. http://rasbt.github.io/biopandas/
+    - We use instead the biopandas library: http://rasbt.github.io/biopandas/
 
     Parameters
     ----------
     pdb_name_in : str
-        PDB file containing the 3D structure of the genome
+        PDB file containing the 3D structure of the genome.
     chromosome_length : list
-        List with chromosome lengths
+        List with chromosome lengths.
     HiC_resolution : int
-        HiC resolution
+        HiC resolution.
     pdb_name_out : str
-        Output PDB file containing the annotated 3D structure of the genome
+        Output PDB file containing the annotated 3D structure of the genome.
     """
-    pdb_coordinates = PandasPdb().read_pdb(pdb_name_in)
-    print(f"Number of beads read from structure: {pdb_coordinates.df['ATOM'].shape[0]}")
+    coordinates = PandasPdb().read_pdb(pdb_name_in)
+    coordinates_df = coordinates.df['ATOM']
+    print(f"Number of beads read from structure: {coordinates_df.shape[0]}")
 
     beads_per_chromosome = [
         math.ceil(length / HiC_resolution) for length in chromosome_length
@@ -123,22 +142,42 @@ def assign_chromosome_number(
     print(
         f"Number of beads deduced from sequence and HiC resolution: {sum(beads_per_chromosome)}"
     )
-
-    # Add residue number
+    # Verify the number of beads in the structure
+    # and the number of beads deduced from the sequence and resolution 
+    # are the same.
+    if coordinates_df.shape[0] != sum(beads_per_chromosome):
+        raise ValueError("Number of beads in structure and "
+                         "number of beads deduced from sequence and resolution "
+                         "are different.")
+    # Add residue (chromosome) number.
     residue_number = []
     for index, bead_number in enumerate(beads_per_chromosome):
         residue_number = residue_number + [index + 1] * bead_number
-    pdb_coordinates.df["ATOM"]["residue_number"] = residue_number
+    coordinates_df["residue_number"] = residue_number
 
-    # Add residue name
-    pdb_coordinates.df["ATOM"]["residue_name"] = "CHR"
+    # Add residue name based on residue number.
+    coordinates_df["residue_name"] = coordinates_df["residue_number"].apply(lambda x: f"C{x:02d}")
 
-    # Fix atom number starting at 1 (instead of 0)
-    pdb_coordinates.df["ATOM"]["atom_number"] = (
-        pdb_coordinates.df["ATOM"]["atom_number"] + 1
+    # Add chain (chromosome) name.
+    # Chromosome 1 -> Chain A
+    # Chromosome 2 -> Chain B
+    # Chromosome 3 -> Chain C
+    # ...
+    letters = string.ascii_uppercase
+    # Handle case where there are more than 26 chromosomes.
+    repeat_factor = math.ceil(len(chromosome_length) / len(letters))
+    residue_to_chain = dict(zip(coordinates_df["residue_number"].unique(), letters * repeat_factor))
+    # Map residue number to chain id.
+    coordinates_df["chain_id"] = coordinates_df["residue_number"].map(residue_to_chain)
+
+    # Fix atom number starting at 1 (instead of 0).
+    coordinates_df["atom_number"] = (
+        coordinates_df["atom_number"] + 1
     )
 
-    pdb_coordinates.to_pdb(
+    # Wirte new PDB file to disk.
+    coordinates.df['ATOM'] = coordinates_df
+    coordinates.to_pdb(
         path=pdb_name_out, records=None, gz=False, append_newline=True
     )
     print(f"Wrote {pdb_name_out}")
@@ -147,8 +186,8 @@ def assign_chromosome_number(
 if __name__ == "__main__":
     ARGS = get_cli_arguments()
 
-    # Read Fasta file and get chromosome length
+    # Read FASTA file and get chromosome length.
     CHROMOSOME_LENGTH = extract_chromosome_length(ARGS.fasta)
 
-    # Assign chromosome number
+    # Assign chromosome number.
     assign_chromosome_number(ARGS.pdb, CHROMOSOME_LENGTH, ARGS.resolution, ARGS.output)
