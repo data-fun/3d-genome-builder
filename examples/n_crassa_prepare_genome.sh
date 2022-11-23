09f7da9b770741cd1d051d98897569196e5fd32a
Pierre Poulain <pierre.poulain@cupnet.net>
2022-11-23 17:06:17 +0100

    Fix typo

--- examples/n_crassa_prepare_genome.sh
+++ examples/n_crassa_prepare_genome.sh
@@ -1,6 +1,6 @@
 # Create target folder
 target_folder="3DGB_n_crassa_WT"
-mkdir -p "${3DGB_n_crassa}"
+mkdir -p "${target_folder}"
 
 # Download genome sequence
 wget https://ftp.ncbi.nlm.nih.gov/geo/series/GSE173nnn/GSE173593/suppl/GSE173593%5Fnc14%5Fgenome%2Dseq%2Dname%2Efasta%2Egz -O "${target_folder}/genome.fasta.gz"
